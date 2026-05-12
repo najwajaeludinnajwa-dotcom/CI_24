95233afbfd7d9370c083e06185e4a87bde25f3c7
najwajaeludinnajwa-dotcom <najwajaeludinnajwa@gmail.com>
2026-05-12 10:54:06 +0700

    pertemuan8
    pertemuan8

--- application/controllers/kategori.php
+++ application/controllers/kategori.php
@@ -7,6 +7,8 @@ class Kategori extends CI_Controller {
     {
     parent::__construct();
     $this->load->model('Kategori_model');
+    if (!$this->session->userdata('login')){
+        redirect()
 }
 
 public function index()
